6f5790955cb01bb85ac4c3961c36e8d9a73ca16a
Hyeryong Choi <chex1004@gmail.com>
2024-07-11 22:42:13 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,8 +1,3 @@
 function solution(s) {
-    return s.split('').sort((a, b) => {
-    if (a.toLowerCase() === a && b.toLowerCase() !== b) return -1;
-    if (a.toLowerCase() !== a && b.toLowerCase() === b) return 1;
-    
-    return a.toLowerCase() < b.toLowerCase() ? 1 : -1; 
-  }).join('');
+    return s.split('').sort().reverse().join('');
 }
