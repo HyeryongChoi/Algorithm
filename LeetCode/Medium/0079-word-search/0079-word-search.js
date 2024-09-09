6b45507b6fc2f000d5a0b4942b1461ece9fec9bb
HyeryongChoi <chex1004@gmail.com>
2024-09-09 11:24:58 +0900

    [Medium] Title: 79. Word Search, Time: 833 ms, Memory: 55.3 MB - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.js
+++ LeetCode/Medium/0079-word-search/0079-word-search.js
@@ -4,55 +4,45 @@
  * @return {boolean}
  */
 
-var exist = function(board, word) {
-    const dy = [-1, 1, 0, 0];
-    const dx = [0, 0, -1, 1];  
-    const startPosList = [];
-    let answer = false;
+var exist = function (board, word) {
+  const dy = [-1, 1, 0, 0];
+  const dx = [0, 0, -1, 1];
+  let answer = false;
 
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++){
-            if(board[i][j] === word[0]) startPosList.push([i,j]);
-        }
+  const visited = Array.from({ length: board.length }, () => Array(board[0].length).fill(false));
+
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[0].length; x++) {
+      visited[y][x] = true;
+      dfs([y, x], board[y][x], visited);
+      visited[y][x] = false;
+
+      if (answer) return true;
+    }
+  }
+
+  return answer;
+
+  function dfs(now, result, visited) {
+    if (result[result.length - 1] !== word[result.length - 1]) return;
+    if (result.length === word.length) {
+      answer = true;
+      return;
     }
 
-    if(word.length === 1) {
-        if(startPosList.length > 0) return true;
-        return false;
+    const [y, x] = now;
+
+    for (let dir = 0; dir < 4; dir++) {
+      const ny = y + dy[dir];
+      const nx = x + dx[dir];
+
+      if (ny < 0 || ny >= board.length) continue;
+      if (nx < 0 || nx >= board[0].length) continue;
+      if (visited[ny][nx]) continue;
+
+      visited[ny][nx] = true;
+      dfs([ny, nx], result + board[ny][nx], visited);
+      visited[ny][nx] = false;
     }
-
-    for(let now of startPosList) {
-        const visited = Array.from({length: board.length}, ()=>Array(board[0].length).fill(false));
-        
-        visited[now[0]][now[1]] = true;
-        dfs(now, word[0], visited);
-
-        if(answer) return true;
-    }
-
-    return answer;
-
-    function dfs(now, result, visited) {
-        if(result === word) {
-            answer = true;
-            return;
-        }
-
-        if(result !== word.slice(0, result.length)) return;
-
-        const [y,x] = now;
-
-        for(let dir = 0; dir < 4; dir++) {
-            const ny = y + dy[dir];
-            const nx = x + dx[dir];
-
-            if(ny < 0 || ny >= board.length) continue;
-            if(nx < 0 || nx >= board[0].length) continue;
-            if(visited[ny][nx]) continue;
-
-            visited[ny][nx] = true;
-            dfs([ny,nx], result+board[ny][nx], visited);
-            visited[ny][nx] = false;
-        }
-    }
+  }
 };
