b42fa58c17faf832078466194cd0537ecd4e07fc
Hyeryong Choi <chex1004@gmail.com>
2024-09-19 20:53:20 +0900

    [Easy] Title: 344. Reverse String, Time: 72 ms, Memory: 57.4 MB - LeetHub

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.js
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.js
@@ -7,7 +7,10 @@ var reverseString = function(s) {
     let end = s.length - 1;
 
     while(start < end) {
-        [s[start++], s[end--]] =[s[end], s[start]];
+        [s[start], s[end]] =[s[end], s[start]];
+
+        start += 1;
+        end -= 1;
     }
 
     return s;
