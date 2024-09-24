ccdb5159faff20c8b65cd0c14515f9536df1d594
Hyeryong Choi <chex1004@gmail.com>
2024-09-24 10:18:25 +0900

    [Easy] Title: 2451. Odd String Difference, Time: 85 ms, Memory: 55.3 MB - LeetHub

--- LeetCode/Easy/2451-odd-string-difference/2451-odd-string-difference.js
+++ LeetCode/Easy/2451-odd-string-difference/2451-odd-string-difference.js
@@ -5,17 +5,19 @@
 var oddString = function(words) {
     const n = words[0].length;
     const count = {};
-    const difference = Array.from({length: words.length}, () => new Array(n-1));
 
     for(let i = 0; i < words.length; i++) {
+        const difference = [];
         for(let j = 0; j < n-1; j++) {
-            difference[i][j] = words[i][j+1].charCodeAt() - words[i][j].charCodeAt();
+            difference.push(words[i][j+1].charCodeAt() - words[i][j].charCodeAt());
 
-            if(count[difference[i]]) count[difference[i]] = [count[difference[i]+1, i]];
-            else count[difference[i]] = [1, i];
+            const key = difference.join('');
+
+            if(count[key]) count[key] = [count[key]+1, i];
+            else count[key] = [1, i];
         }
     }
-    
+
     for(let [cnt, index] of Object.values(count)) {
         if(cnt === 1) return words[index];
     }
