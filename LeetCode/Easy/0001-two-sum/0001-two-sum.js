44015cdaff52dcaceb119d78879461bbca7a22da
Hyeryong Choi <chex1004@gmail.com>
2024-10-02 21:37:19 +0900

    [Easy] Title: 1. Two Sum, Time: 99 ms, Memory: 49.5 MB - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.js
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.js
@@ -5,12 +5,8 @@
  */
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
-        for(let j = 0; j < nums.length; j++) {
-            if(i === j) continue;
-
-            const sum = nums[i] + nums[j];
-
-            if(sum === target) return [i,j];
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) return [i, j];
         }
     }
 };
