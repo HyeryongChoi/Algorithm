fe2d792b0780aacc7d28caa63830e5c61a5d201d
Hyeryong Choi <chex1004@gmail.com>
2024-10-02 22:28:47 +0900

    [Easy] Title: 219. Contains Duplicate II, Time: 112 ms, Memory: 69.2 MB - LeetHub

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -7,22 +7,13 @@ var containsNearbyDuplicate = function(nums, k) {
     const indexObj = {};
 
     for(let i = 0; i < nums.length; i++) {
-        if(!indexObj[nums[i]]) indexObj[nums[i]] = [];
-        
-        indexObj[nums[i]].push(i);
+        if(indexObj[nums[i]] >= 0) {
+            const diff = Math.abs(i - indexObj[nums[i]]);
 
-        if(indexObj[nums[i]]?.length >= 2) {
-            let [start, end] = [0, 1];
-            let indexList = indexObj[nums[i]];
-
-            while(start < end && end < indexList.length) {
-                if (Math.abs(indexList[start] - indexList[end]) <= k) return true;
-                else {
-                    start += 1;
-                    end += 1;
-                }
-            }
+            if(diff <= k) return true;
         }
+
+        indexObj[nums[i]] = i;
     }
 
     return false;
