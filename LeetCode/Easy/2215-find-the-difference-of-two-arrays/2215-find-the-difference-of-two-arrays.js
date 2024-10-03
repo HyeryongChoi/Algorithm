68332e3f15b0b1520107299b5c9a3584781979bb
Hyeryong Choi <chex1004@gmail.com>
2024-10-03 18:39:31 +0900

    [Easy] Title: 2215. Find the Difference of Two Arrays, Time: 137 ms, Memory: 55.8 MB - LeetHub

--- LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ LeetCode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,16 +4,16 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    const uniq1 = [];
-    const uniq2 = [];
+    const uniq1 = {};
+    const uniq2 = {};
 
     nums1.forEach(num => {
-        if(!nums2.includes(num) && !uniq1.includes(num)) uniq1.push(num);
+        if(!nums2.includes(num) && !uniq1[num]) uniq1[num] = true;
     });
 
     nums2.forEach(num => {
-        if(!nums1.includes(num) && !uniq2.includes(num)) uniq2.push(num);
+        if(!nums1.includes(num) && !uniq2[num]) uniq2[num] = true;
     });
 
-    return [uniq1, uniq2];
+    return [Object.keys(uniq1).map(Number), Object.keys(uniq2).map(Number)];
 };
